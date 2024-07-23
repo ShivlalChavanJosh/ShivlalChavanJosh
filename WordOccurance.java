643f0256ad386d02dfd2c18182fc3903ea421c31
Shivlal Chavan <101095938+ShivlalChavanJosh@users.noreply.github.com>
2024-07-23 14:59:52 +0530

    Update WordOccurance.java

--- WordOccurance.java
+++ WordOccurance.java
@@ -1,36 +1,36 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class WordCounter {
+class WordCounter {
     public static void main(String[] args) {
-        String statement = "people love to love people";
-        countWordOccurrences(statement);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the statement:-");
+        String statement = sc.nextLine();
+        sc.close();
+        checkoccurance(statement);
+      
     }
-
-    public static void countWordOccurrences(String statement) {
-        // Split the statement into words
+    
+    public static void checkoccurance(String statement){
+        
         String[] words = statement.split("\\s+");
 
-        // Create a map to store the word count
-        Map<String, Integer> wordCountMap = new HashMap<>();
+        Map<String,Integer> wordsCount = new HashMap<>();
 
-        // Iterate through each word in the array
-        for (String word : words) {
-            // Convert word to lowercase to make counting case insensitive
+        for(String word: words){
             word = word.toLowerCase();
 
-            // If the word is already in the map, increment its count
-            if (wordCountMap.containsKey(word)) {
-                wordCountMap.put(word, wordCountMap.get(word) + 1);
-            } else {
-                // If the word is not in the map, add it with a count of 1
-                wordCountMap.put(word, 1);
-            }
+             if(wordsCount.containsKey(word)){
+                 wordsCount.put(word,(wordsCount.get(word)+1));
+             }else{
+                 wordsCount.put(word,1);
+             } 
         }
 
-        // Print the word counts
-        for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+        for(Map.Entry<String,Integer> entry: wordsCount.entrySet()){
+            System.out.println(entry.getKey() + ":"+ entry.getValue());
         }
+        
+
     }
 }
